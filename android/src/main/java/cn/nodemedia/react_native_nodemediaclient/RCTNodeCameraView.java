fc73fe4630ad076e22f10c51603931e6d8730db4
qwikcut <jonathankerbelis@gmail.com>
2020-09-15 17:16:39 -0400

    logging updates

--- android/src/main/java/cn/nodemedia/react_native_nodemediaclient/RCTNodeCameraView.java
+++ android/src/main/java/cn/nodemedia/react_native_nodemediaclient/RCTNodeCameraView.java
@@ -105,7 +105,7 @@ public class RCTNodeCameraView extends NodeCameraView implements LifecycleEventL
     public void setZoomScale(float zoomScale) {
         mNodePublisher.setZoom(zoomScale);
         this.zoomScale = zoomScale;
-        System.out.println("TESTING: zoomScale - " + this.zoomScale);
+        Log.d("RCTNodeCameraView", "zoomScale: " + zoomScale);
     }
 
     public int startPrev() {
